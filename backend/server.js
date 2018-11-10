28d9a33145086333b566a71d36fcfbd841ed27bb
TMisch <taylormisch@gmail.com>
2018-11-10 17:57:07 -0600

    added database

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const Data = require("./data");
+
+const API_PORT = 3001;
+const app = express();
+const router = express.Router();
+// import environment variables from .env file
+require("dotenv").config();
+
+// this is our MongoDB database
+const dbRoute =
+  "mongodb://" +
+  process.env.DB_USER +
+  ":" +
+  process.env.DB_PASSWORD +
+  "@" +
+  process.env.DB_HOST;
+
+// connects our back end code with the database
+mongoose.connect(
+  dbRoute,
+  { useNewUrlParser: true }
+);
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+
+// checks if connection with the database is successful
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// (optional) only made for logging and
+// bodyParser, parses the request body to be a readable json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger("dev"));
+
+// append /api for our http requests
+app.use("/api", router);
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
