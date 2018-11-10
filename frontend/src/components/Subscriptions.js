d0c8a879dbabed266df536ca4542cd9695c9690f
Rachel Korkowski <rachel.korkowski@gmail.com>
2018-11-10 15:26:48 -0600

    Setup Routes

---
+++ frontend/src/components/Subscriptions.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Subscriptions = () => {
+    return (
+        <div>
+            <p>Subscriptions</p>
+        </div>
+    );
+};
+
+export default Subscriptions;
