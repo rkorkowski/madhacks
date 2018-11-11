0d99316c05eef0fe38c53f128d52ebf0f195050c
Rachel Korkowski <rachel.korkowski@gmail.com>
2018-11-11 10:42:14 -0600

    Fixed login link

--- frontend/src/components/navigation/Navigation.js
+++ frontend/src/components/navigation/Navigation.js
@@ -22,7 +22,7 @@ const Navigation = props => {
                 <ul>
                     <li><a href="/">Home</a></li>
                     <li><a href="/subscriptions">Subscriptions</a></li>
-                    <li><a href="/">Login</a></li>
+                    <li><a href="/login">Login</a></li>
                 </ul>
             </div>
             
